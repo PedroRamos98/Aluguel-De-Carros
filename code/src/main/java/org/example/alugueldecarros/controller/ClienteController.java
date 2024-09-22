f7561a2165a53276784a184091f85bd7f92112e8
RafaelPchequer <rafaelpchequer@gmail.com>
2024-09-22 14:56:32 -0300

    back: fixed db

--- code/src/main/java/org/example/alugueldecarros/controller/ClienteController.java
+++ code/src/main/java/org/example/alugueldecarros/controller/ClienteController.java
@@ -1,7 +1,11 @@
+package org.example.alugueldecarros.controller;
+
+import org.example.alugueldecarros.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.example.alugueldecarros.model.Cliente;
 
 import java.util.List;
 
